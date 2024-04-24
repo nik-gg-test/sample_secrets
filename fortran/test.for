caef610862ba81e1b00b75cce8cd32d644f37895
nik-gg-test <159565030+nik-gg-test@users.noreply.github.com>
2024-04-24 11:29:57 -0400

    Update test.for

--- fortran/test.for
+++ fortran/test.for
@@ -5,12 +5,3 @@ test
 $userid = "UN660579YV"
 $passwd = "AWS6835737778334fHHYUU96867335&"
 
-$another-userid = "FRED15723V"
-$another-passwd = "AWS658577d997434fHHYUU99867335&"
-
-$another-userid = "FRED55575V"
-$another-passwd = "AWS658577d99367788UU99867335&"
-
-
-$another-userid = "FRED65686V"
-$another-passwd = "AWS658577d99386778UU9876544569867335&"
