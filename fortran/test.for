361cd72bf3eed2d60e74d3fd7f9538cf0146f246
nik-gg-test <159565030+nik-gg-test@users.noreply.github.com>
2024-04-24 11:28:36 -0400

    Update test.for

--- fortran/test.for
+++ fortran/test.for
@@ -1,1 +1,16 @@
 test
+#this is a test using filepath exclusions
+
+
+$userid = "UN660579YV"
+$passwd = "AWS6835737778334fHHYUU96867335&"
+
+$another-userid = "FRED15723V"
+$another-passwd = "AWS658577d997434fHHYUU99867335&"
+
+$another-userid = "FRED55575V"
+$another-passwd = "AWS658577d99367788UU99867335&"
+
+
+$another-userid = "FRED65686V"
+$another-passwd = "AWS658577d99386778UU9876544569867335&"
